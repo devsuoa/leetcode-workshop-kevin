4e5b0c6391445bd5ab8dce7fb07dc662e139597e
KeiHara <84754523+KeiHara@users.noreply.github.com>
2023-03-30 17:29:40 +1300

    update

--- exercise.py
+++ exercise.py
@@ -33,18 +33,7 @@ def convert_neighbour(arr, i, j):
 
 
 def dfs(arr, i, j, visited, moves, n, m):
-    if arr[i][j] == 0:
-        return
-
-    arr[i][j] = 2
-    visited[i][j] = True
-
-    for move in moves:
-        i_next = i + move[0]
-        j_next = j + move[1]
-
-        if in_range(n, m, i_next, j_next) and not visited[i_next][j_next]:
-            dfs(arr, i_next, j_next, visited, moves, n, m)
+    # implement this function
 
 
 def in_range(n, m, i, j):
